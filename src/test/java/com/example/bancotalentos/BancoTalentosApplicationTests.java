e5711bea1ecd4a5568b7f84e81cefaa46b970358
SamuelJBSilva <80862761+S14-cmd@users.noreply.github.com>
2023-04-24 15:08:39 -0300

    Add files via upload

---
+++ src/test/java/com/example/bancotalentos/BancoTalentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bancotalentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoTalentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
